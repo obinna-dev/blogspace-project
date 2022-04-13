a4deb17fbd71d334919353cdcca16210cb2446b8
iamthefastone <o.osisiogu@gmail.com>
2022-04-13 18:01:02 +0100

    Post blog post to API and update DOM

--- index.js
+++ index.js
@@ -1,30 +1,44 @@
+let postsArr = []
+
+function renderPosts() {
+    let html = ""
+        for (let post of postsArr) {
+            html += `
+                <h3>${post.title}</h3>
+                <p>${post.body}</p>
+                <hr />
+            `
+        }
+        document.getElementById("blog-list").innerHTML = html
+}
+
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(res => res.json())
     .then(data => {
-    let postsArr = data.slice(0, 5)
-        console.log(postsArr)
-    let postsHtml = ""
-    for (let post of postsArr) {
-        postsHtml += `
-            <h3>${post.title}</h3>
-            <p>${post.body}</p>
-            <hr />
-        `
-        document.getElementById("posts-container").innerHTML = postsHtml
+        postsArr = data.slice(0, 5)
+        renderPosts()
+    })
+
+document.getElementById("new-post").addEventListener("submit", (e) => {
+    e.preventDefault()
+    const postTitle = document.getElementById("post-title").value
+    const postBody = document.getElementById("post-body").value
+    const data = {
+        title: postTitle,
+        body: postBody
     }
-})
 
-
-// fetch("https://apis.scrimba.com/jsonplaceholder/posts")
-//     .then(res => res.json())
-//     .then(data => {
-//     let postsArr = data.slice(0, 5)
-//     postsArr.map(function(post){
-//         postsHtml = `
-//             <h3>${post.title}</h3>
-//             <p>${post.body}</p>
-//             <hr />
-//         `
-//         document.getElementById("posts-container").innerHTML += postsHtml
-//     })
-// })
+    const options = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)      
+    .then(res => res.json())
+    .then(data => {
+        postsArr.unshift(data)
+        renderPosts()
+    })    
+ })
